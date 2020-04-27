86872f81869b32a25485572479740aa08a40d5f8
lengea <43341984+lengea@users.noreply.github.com>
2020-04-27 11:50:36 +0700

    Second Commit

--- flask_oukas.py
+++ flask_oukas.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, request, flash
+
+"""redirect"""
+
 """from forms import RegistrationForm, LoginForm"""
+
+"""from forms import SignupForm"""
+
+from forms import ContactForm
+
+from flask_mail import Message, Mail
+
+mail = Mail()
 
 app = Flask(__name__)
 
 """Protect against modified cookies and cross site requests.
 Secret key from secrets.token_hex(16)"""
 app.config['SECRET_KEY'] = '32147925445725911e18258e8942f416'
+ 
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'chungleng2015@gmail.com'
+"""Account that will be used to send emails from"""
+app.config["MAIL_PASSWORD"] = ''
+"""Fill in real password later"""
+
+mail.init_app(app)
 
 @app.route('/')
 @app.route('/home')
@@ -26,7 +47,7 @@ def apply():
     return render_template('Oukas_Apply.html')
 
 
-@app.route("/register", methods=['GET', 'POST'])
+"""@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -44,7 +65,39 @@ def login():
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('Oukas_Login.html', title='Login', form=form)
+    return render_template('Oukas_Login.html', title='Login', form=form)"""
+
+"""
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.is_submitted():
+        result = request.form
+        return render_template('Oukas_User.html', result=result)
+    return render_template('Oukas_Signup.html', form=form)
+"""
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+  form = ContactForm()
+ 
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('Oukas_Contact.html', form=form)
+    else:
+      msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
+      msg.body = """
+      From: %s &lt;%s&gt;
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+ 
+      return render_template('Oukas_Contact.html', success=True)
+ 
+  elif request.method == 'GET':
+    return render_template('Oukas_Contact.html', form=form)
 
 """
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,4 +1,13 @@
-from flask_wtf import FlaskForm
+"""from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+class SignupForm(FlaskForm):
+    username = StringField('Username')
+    password = StringField('Password')
+    submit = SubmitField('Sign Up')"""
+
+
+"""from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
@@ -18,4 +27,16 @@ class LoginForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    submit = SubmitField('Login')"""
+
+from flask_wtf import Form
+from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError
+
+class ContactForm(Form):
+  name = TextField("Name",  [validators.Required("Please enter your name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  """.Email checks if email provided matches the user@example.com format"""
+  subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
+  message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
+  submit = SubmitField("Send")
+ 
